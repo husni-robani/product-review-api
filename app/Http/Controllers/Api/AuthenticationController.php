f79d9739a5b86c133deab97dabbf519081372c9f
husni-robani <husnir2005@gmail.com>
2024-10-11 21:47:20 +0700

    Create controller for authentication api

---
+++ app/Http/Controllers/Api/AuthenticationController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\ApiResponse;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\Api\AuthenticateRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
+
+class AuthenticationController extends Controller
+{
+    /**
+     *This method is for issuing token
+     * @param AuthenticateRequest $request
+     * @return ApiResponse
+     */
+    public function authenticate(AuthenticateRequest  $request){
+
+        $user = User::where('email', $request->get('email'))->first();
+
+        if (!$user || $request->get('password') != $user->password) {
+            return new ApiResponse(401, errorMessage: 'Email or password is incorrect');
+        }
+
+        $token = $user->createToken('secret_token');
+        return new ApiResponse(
+            200,
+            data: ['token' => $token->plainTextToken],
+            message: 'Login Success!'
+        );
+    }
+}
