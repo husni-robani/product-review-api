1a891ac051bc8514bf9bed4dc020928ceab1ba19
husni-robani <husnir2005@gmail.com>
2024-10-12 21:12:17 +0700

    create request for create new review and update review

---
+++ app/Http/Requests/Api/StoreReviewRequest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use App\Http\ApiResponse;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class StoreReviewRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'product_id' => 'required|numeric',
+            'review_text' => 'required|string',
+            'rate' => 'required|between:1,5'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $response = new ApiResponse(422, errorMessage: $validator->errors()->all());
+        throw new HttpResponseException(($response->toResponse($this->input())));
+    }
+}
---
+++ app/Http/Requests/Api/UpdateReviewRequest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use App\Http\ApiResponse;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UpdateReviewRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'review_text' => 'string',
+            'rate' => 'between:1,5'
+        ];
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+        $response = new ApiResponse(422, errorMessage: $validator->errors()->all());
+        throw new HttpResponseException(($response->toResponse($this->input())));
+    }
+}
