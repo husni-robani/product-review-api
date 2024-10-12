ab67527780f5a192f4ea0364939275ab0d02f8d7
husni-robani <husnir2005@gmail.com>
2024-10-12 14:36:31 +0700

    Create migration and model for reviews table

---
+++ database/migrations/2024_10_12_071514_create_reviews_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReviewsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reviews', function (Blueprint $table) {
+            $table->id();
+            $table->text('review_text');
+            $table->enum('rate', [1, 2, 3, 4, 5]);
+            $table->foreignId('productId')->references('id')->on('products')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reviews');
+    }
+}
