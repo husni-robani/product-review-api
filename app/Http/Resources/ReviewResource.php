73c7bddca2c202bf293aa54fdf8920fa9767e6b5
husni-robani <husnir2005@gmail.com>
2024-10-12 20:40:01 +0700

    modify ReviewResource.php

--- app/Http/Resources/ReviewResource.php
+++ app/Http/Resources/ReviewResource.php
@@ -2,22 +2,46 @@
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+
 
 class ReviewResource extends JsonResource
 {
+    protected static bool $includeProduct = false;
+
+    /**
+     * Set is data include product or not
+     *
+     * @param bool $includeProduct
+     * @return void
+     */
+    public static function includeProduct(bool $includeProduct): void
+    {
+        self::$includeProduct = $includeProduct;
+    }
+
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     * @param  Request  $request
+     * @return array
      */
-    public function toArray($request)
+    public function toArray($request): array
     {
-        return [
-          'id' => $this->id,
-          'review' => $this->review_text,
-          'rate' => $this->rate
+        $data =  [
+            'id' => $this->id,
+            'review' => $this->review_text,
+            'rate' => $this->rate
         ];
+
+        if (self::$includeProduct){
+            $data['product'] = [
+                'id' => $this->product->id,
+                'name' => $this->product->name
+            ];
+        }
+        return $data;
+
     }
 }
