f45cc62e1bea4cd9389eb2643b1c2663cfe2876b
husni-robani <husnir2005@gmail.com>
2024-10-13 16:11:35 +0700

    Add routes for web

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,37 @@
 <?php
 
+use App\Http\Controllers\AuthenticationController;
+use App\Http\Controllers\ProductController;
+use App\Http\Controllers\ReviewController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::controller(AuthenticationController::class)->group(function() {
+    Route::get('/login', 'login')->name('login');
+    Route::post('/login', 'authenticate')->name('authenticate');
+});
+
+Route::middleware('auth')->group(function() {
+    Route::get('/logout', [AuthenticationController::class, 'logout'])->name('logout');
+
+    Route::controller(ProductController::class)->group(function() {
+        Route::get('/products', 'index')->name('products');
+        Route::get('/products/create', 'create')->name('products.create');
+        Route::post('/products/create', 'store')->name('products.store');
+        Route::get('/products/edit/{productId}', 'edit')->name('products.edit');
+        Route::patch('/products/edit/{productId}', 'update')->name('products.update');
+        Route::delete('/products/{productId}', 'destroy')->name('products.delete');
+    });
+
+    Route::controller(ReviewController::class)->group(function () {
+        Route::get('/reviews', 'index')->name('reviews');
+        Route::get('/reviews/create', 'create')->name('reviews.create');
+        Route::post('/reviews/create', 'store')->name('reviews.store');
+        Route::get('/reviews/edit/{reviewId}', 'edit')->name('reviews.edit');
+        Route::patch('/reviews/edit/{reviewId}', 'update')->name('reviews.update');
+        Route::delete('/reviews/{reviewId}', 'destroy')->name('reviews.destroy');
+    });
 });
